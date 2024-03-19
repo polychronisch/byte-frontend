575c5d283275136e84847641b3aedb059221cf2d
polychronis charalampidis <polis9685@gmail.com>
2024-03-20 00:22:54 +0200

    delete degrees details

--- src/components/Degrees.js
+++ src/components/Degrees.js
@@ -1,8 +1,21 @@
 import { useState,useEffect } from "react";
 import { Button, Stack,Table } from "react-bootstrap"
+import axios from 'axios';
+
 export default function Degrees(){
     const [degrees,setDegrees] = useState([])
     const [degreesUsed,setDegreesUsed] = useState([])
+    const deleteCandidate = async(id)=>{
+        try {
+            const response = await axios.post('http://localhost:8000/api/delete-degree/'+id);
+            
+            // Optionally, you can handle success response here
+        } catch (error) {
+            // console.error(error.response.data);
+            // Optionally, you can handle error response here
+        }
+        window.location.reload();
+    }
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -34,7 +47,7 @@ export default function Degrees(){
                                 <td> 
                                     <p>{degree.degreeTitle}</p>
                                     {!degreesUsed.some(item => item.degree_id === degree.id) && ( 
-                                        <Button variant='danger'>Delete</Button>
+                                        <Button variant='danger' onClick={ () => deleteCandidate(degree.id)}>Delete</Button>
                                     )}
                                 </td>
                             </tr>
